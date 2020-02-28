d6662b48e3f72b53181d98a75a91a29ee33e29e3
Erik Schierboom <erik_schierboom@hotmail.com>
2020-02-28 17:15:45 +0100

    More work

--- languages/fsharp/exercises/concept/numbers-floating-point/NumbersFloatingPoint.fs
+++ languages/fsharp/exercises/concept/numbers-floating-point/NumbersFloatingPoint.fs
@@ -1,10 +1,29 @@
 module NumbersFloatingPoint
 
-let annualPercentageYield (balance: decimal): float =
-    failwith "Please implement the 'annualPercentageYield' function"
+let annualPercentageYield (balance: decimal): single =
+    if balance < 0.0m then
+        -3.213f
+    elif balance < 1000.0m then
+        0.5f
+    elif balance < 5000.0m then
+        1.621f
+    else
+        2.475f
+        
+let private annualYield (balance: decimal): decimal =
+    let multiplier =  decimal (annualPercentageYield balance) / 100.0m
+    abs balance * multiplier
     
 let annualBalanceUpdate (balance: decimal): decimal =
-    failwith "Please implement the 'annualBalanceUpdate' function"
+    balance + annualYield balance
     
 let yearsBeforeDesiredBalance (balance: decimal) (targetBalance: decimal): int =
-    failwith "Please implement the 'yearsBeforeDesiredBalance' function" 
+    let mutable accumulatingBalance = balance
+    let mutable years = 0
+
+    while accumulatingBalance < targetBalance do
+        accumulatingBalance <- annualBalanceUpdate accumulatingBalance
+        years <- years + 1
+
+    years
+   
